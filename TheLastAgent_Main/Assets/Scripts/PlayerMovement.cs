26a8054a85038a755e52c83b458a1afde7aa3d6e
SamjH0ward <91006816+SamjH0ward@users.noreply.github.com>
2022-02-03 12:59:26 +0000

    made a basic movement script and camera follow using cinemachine

---
+++ TheLastAgent_Main/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private float movementSpeed = 8.0f;
+
+    private Vector2 movementInput;
+    private Rigidbody2D rb2d;
+        
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>(); 
+    }
+
+    private void OnMove(InputValue input)
+    {
+        movementInput = input.Get<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb2d.velocity = movementInput * movementSpeed;
+    }
+}
