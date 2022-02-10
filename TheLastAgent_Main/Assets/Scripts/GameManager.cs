67cd3e4debf67bc8e56a01769713b43b1158371a
SamjH0ward <91006816+SamjH0ward@users.noreply.github.com>
2022-02-10 15:09:24 +0000

    added score

--- TheLastAgent_Main/Assets/Scripts/GameManager.cs
+++ TheLastAgent_Main/Assets/Scripts/GameManager.cs
@@ -1,26 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using System;
+
 
 public class GameManager : MonoBehaviour
 {
-    private int _score = 0;
-    private PickUp score;
-
-    protected virtual void Awake()
-    {
-        score = GetComponent<PickUp>();
-    }
-
-    private void OnEnable() => score.OnPickUpCollected += PickUpCollected;
-    private void OnDisable() => score.OnPickUpCollected -= PickUpCollected;
-
-    private void PickUpCollected()
-    {
-        _score += 1;
-        Debug.Log("The current score is " + _score.ToString());
-    }
-
+    [SerializeField] public static int score = 0;
 
 }
--- TheLastAgent_Main/Assets/Scripts/PickUp.cs
+++ TheLastAgent_Main/Assets/Scripts/PickUp.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using System;
 
@@ -11,11 +10,13 @@ public class PickUp : MonoBehaviour
 
         if (collision.gameObject.CompareTag("Player"))
         {
-            OnPickUpCollected?.Invoke();
             gameObject.SetActive(false);
+            GameManager.score += 1;
+            
             
         }
     }
 
+    
     
 }
